f2d22ad6d8c1377f75e01db764881b9ca560a798
Josey Morton <me@joseymorton.com>
2013-01-02 16:00:17 -0600

    proper filters and tests
    using proper filterable method for read and write, making tests pass

--- tests/cases/extensions/adapter/storage/filesystem/S3Test.php
+++ tests/cases/extensions/adapter/storage/filesystem/S3Test.php
@@ -6,29 +6,30 @@ use li3_aws\extensions\adapter\storage\filesystem\S3;
 
 class S3Test extends \lithium\test\Unit {
     protected $configuration = array(
-        'bucket' => 'li3_aws_test'
-    );
+		'bucket' => 'li3awstest',
+	);
 
-    public function setUp() {
-        $this->s3 = new S3($this->configuration);
-    }
+	public function setUp() {
+		$this->s3 = new S3($this->configuration);
+	}
 
-    public function tearDown() {
-        unset($this->s3);
-    }
+	public function tearDown() {
+		unset($this->s3);
+	}
 
-    public function testSimpleWrite() {
-        $filename = 'test_file';
-        $data = 'test data';
+	public function testSimpleWrite() {
+		$filename = 'test_file';
+		$data = 'test data';
 
-        $closure = $this->s3->write($filename, $data);
-        $this->assertTrue(is_callable($closure));
+		$params = compact('filename', 'data');
+		$result = $this->s3->write($filename, $data);
+		$this->assertTrue($result);
 
-        $params = compact('filename', 'data');
-        $result = $closure($this->s3, $params, null);
-        $this->assertTrue($result);
+	}
 
-        $result = file_get_contents('http://s3.amazonaws.com/'.$this->configuration['bucket'].'/'.$filename);
-        $this->assertEqual($data, $result);
-    }
+	public function testSimpleRead(){
+		$filename = 'test_file';
+		print_r($this->s3->read($filename, array('url_only' => true)));
+	}
+
 }
